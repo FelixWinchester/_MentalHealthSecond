fd944bd8c1a3093ce732ff8ff0a8798298bcd16c
FoWaX <sinelnikovnikita817@gmail.com>
2025-04-28 01:23:55 +0400

    based_backend_Graph

--- backend/models.py
+++ backend/models.py
@@ -17,7 +17,25 @@ class MoodType(StrEnum):
     TIRED = "tired"
     EXCITED = "excited"
     ANXIOUS = "anxious"
+    
+#класс для работы графика
+class MoodChartPoint(BaseModel):
+    date: datetime
+    mood: str
 
+    class Config:
+        from_attributes = True
+
+class MoodMap:
+    _mood_to_score = {
+        "HAPPY": 5,
+        "EXCITED": 5,
+        "CALM": 4,
+        "TIRED": 3,
+        "SAD": 2,
+        "ANXIOUS": 2,
+        "ANGRY": 1,
+    }
 # Модели Pydantic для валидации данных
 
 class UserBase(BaseModel):
